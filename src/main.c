197c12e5c5958c9aa4f6268347425a57a4bd4048
Thomaswalsh1 <130932404+thomaswalsh1@users.noreply.github.com.>
2026-01-20 14:37:40 -0500

    Main Loop documentation

--- src/main.c
+++ src/main.c
@@ -6,17 +6,20 @@
 #include "ui/screens.h"
 
 int main(void) {
+    // ncurses initialization
     initscr();
     raw();
     keypad(stdscr, TRUE);
     noecho();
 
+    // initializing project root
     init_project_root();
 
-    // Load exercises from config files
+    // generate exercise list from configuration files
+    // warning from 01/20/26: hardcoded path, should change in future versions
     ExerciseList exercise_list = load_exercises_from_config("labs_reprise/grep/config");
-    exercises = exercise_list.exercises;
-    exercise_count = exercise_list.count;
+    exercises = exercise_list.exercises; // unloading exercises to global variable
+    exercise_count = exercise_list.count; // unloading exercise count to global variable
 
     if (exercise_count == 0) {
         endwin();
@@ -27,18 +30,17 @@ int main(void) {
     int title_status = run_title();
     if (title_status == ACTION_EXIT) {
         endwin();
-        free_exercise_list(exercise_list);
+        free_exercise_list(exercise_list); // free allocated memory using utility function
         return 0;
     }
     int explanation_status = run_explanation();
     if (explanation_status == ACTION_EXIT){
         endwin();
-        free_exercise_list(exercise_list);
+        free_exercise_list(exercise_list); // free allocated memory using utility function
         return 0;
     }
 
     for (int i = 0; i < exercise_count; i++) {
-        // printf("i is: %d", i);
         int action = run_exercise(&exercises[i]); // we run the exercise according to the exercise array
         if (action == ACTION_EXIT) break;
 
