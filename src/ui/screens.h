b41d27792c5c60996c16529869c1f0ac5774a135
Thomaswalsh1 <130932404+thomaswalsh1@users.noreply.github.com.>
2026-01-22 11:32:34 -0500

    UI screens documentation

--- src/ui/screens.h
+++ src/ui/screens.h
@@ -2,7 +2,15 @@
 #ifndef SCREENS_H
 #define SCREENS_H
 
+/**
+ * UI helper function that displays the success page
+ */
 void show_success(void);
+
+/**
+ * UI helper function that displays the failure page
+ */
 void show_failure(const char *hint);
+
 
 #endif
